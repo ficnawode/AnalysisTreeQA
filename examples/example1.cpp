e642a0911fcd7d5f35f8d94d50eb08fe3446b59c
Tobiasz Fic <tobi.fic@gmail.com>
2023-12-16 11:28:28 +0100

    VtxTracks -> VtxTracks_pred

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -42,29 +42,29 @@ void example(const std::string& filelist) {
     static constexpr int nbdtbins = 1000;
     std::string j_str = std::to_string(j);
     auto hist_title = "BDT " + particle_names[j] + " , %";
-    auto bdt_var_name = "VtxTracks.onnx_pred_0_" + j_str;
+    auto bdt_var_name = "VtxTracks_pred.onnx_pred_0_" + j_str;
     auto bdt_var = Variable::FromString(bdt_var_name);
     bdt_var.SetName("BDT_" + particle_names[j] + '_' + std::to_string(j));
     task->AddH1({hist_title, bdt_var, {nbdtbins, 0, 1}});
   }
 
-  task->AddH1({"momentum p, []", Variable::FromString("VtxTracks.p"), {200, -6, 6}});
-  task->AddH1({"pid, []", Variable::FromString("VtxTracks.mc_pdg"), {200, -6, 6}});
+  task->AddH1({"momentum p, []", Variable::FromString("VtxTracks_pred.p"), {200, -6, 6}});
+  task->AddH1({"pid, []", Variable::FromString("VtxTracks_pred.mc_pdg"), {200, -6, 6}});
 
-  Variable p_over_q = Variable("pq", {{"VtxTracks", "p"}, {"VtxTracks", "q"}}, [](std::vector<double>& var) { return var.at(0) / var.at(1); });
+  Variable p_over_q = Variable("pq", {{"VtxTracks_pred", "p"}, {"VtxTracks_pred", "q"}}, [](std::vector<double>& var) { return var.at(0) / var.at(1); });
   auto mass2 = Variable::FromString("TofHits.mass2");
 
   mass2.SetName("m2");
   task->AddH2({"p/q, (GeV/c)", p_over_q, {400, -12, 12}}, {"m^{2}, (GeV^{2}/c^{4})", mass2, {400, -1, 2}});
 
-  auto q_cut = RangeCut("VtxTracks.q", 0, 2);
+  auto q_cut = RangeCut("VtxTracks_pred.q", 0, 2);
 
   // PROTONS
   {
-    auto bdt_cut = RangeCut("VtxTracks.onnx_pred_0_0", 0.999, 1.1);
+    auto bdt_cut = RangeCut("VtxTracks_pred.onnx_pred_0_0", 0.999, 1.1);
     int pdg = 2212;
-    auto simpdg_cut = EqualsCut("VtxTracks.mc_pdg", pdg);
-    auto wrong_pid_cut = CreateNotEqualsCut("VtxTracks.mc_pdg", pdg);
+    auto simpdg_cut = EqualsCut("VtxTracks_pred.mc_pdg", pdg);
+    auto wrong_pid_cut = CreateNotEqualsCut("VtxTracks_pred.mc_pdg", pdg);
 
     Cuts* xgb_cut = new Cuts("P_xgb", {bdt_cut, q_cut});
     Cuts* all_simulated_cut = new Cuts("P_all_simulated", {simpdg_cut});
@@ -84,10 +84,10 @@ void example(const std::string& filelist) {
 
   // kaons
   {
-    auto bdt_cut = RangeCut("VtxTracks.onnx_pred_0_1", 0.999, 1.1);
+    auto bdt_cut = RangeCut("VtxTracks_pred.onnx_pred_0_1", 0.999, 1.1);
     int pdg = 321;
-    auto simpdg_cut = EqualsCut("VtxTracks.mc_pdg", pdg);
-    auto wrong_pid_cut = CreateNotEqualsCut("VtxTracks.mc_pdg", pdg);
+    auto simpdg_cut = EqualsCut("VtxTracks_pred.mc_pdg", pdg);
+    auto wrong_pid_cut = CreateNotEqualsCut("VtxTracks_pred.mc_pdg", pdg);
 
     Cuts* xgb_cut = new Cuts("K+_xgb", {bdt_cut, q_cut});
     Cuts* all_simulated_cut = new Cuts("K+_all_simulated", {simpdg_cut});
@@ -107,10 +107,10 @@ void example(const std::string& filelist) {
 
   // PROTONS
   {
-    auto bdt_cut = RangeCut("VtxTracks.onnx_pred_0_2", 0.999, 1.1);
+    auto bdt_cut = RangeCut("VtxTracks_pred.onnx_pred_0_2", 0.999, 1.1);
     std::vector<double> pdg{-13, 211, -11};
-    auto simpdg_cut = CreateEqualsVectorCut("VtxTracks.mc_pdg", pdg);
-    auto wrong_pid_cut = CreateNotEqualsVectorCut("VtxTracks.mc_pdg", pdg);
+    auto simpdg_cut = CreateEqualsVectorCut("VtxTracks_pred.mc_pdg", pdg);
+    auto wrong_pid_cut = CreateNotEqualsVectorCut("VtxTracks_pred.mc_pdg", pdg);
 
     Cuts* xgb_cut = new Cuts("Pi+_xgb", {bdt_cut, q_cut});
     Cuts* all_simulated_cut = new Cuts("Pi+_all_simulated", {simpdg_cut});
